a4e13f2d3fd0f8151dcb8574b1ed88b4d2641b5b
Md Asif Alam <mdasifalam910@gmail.com>
2023-09-27 16:26:49 +0530

    first commit

---
+++ fatarrow.js
@@ -0,0 +1,20 @@
+console.log("Fat Arrow functions");
+
+//ES5
+// var sum = function () {
+//   var a = 10;
+//   var b = 5;
+//   return a + b;
+// };
+
+// console.log(sum());
+
+//ES6
+
+const sum = () => {
+  let a = 10;
+  let b = 20;
+  return a + b;
+};
+
+console.log(sum());
